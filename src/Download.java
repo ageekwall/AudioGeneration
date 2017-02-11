46d872dfd16ce8966fec27a802720fb51f45e598
ageekwall <agrawalsoumya94@gmail.com>
2017-02-11 17:29:29 +0530

    Upload & Download file

---
+++ src/Download.java
@@ -0,0 +1,56 @@
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Download
+ */
+public class Download extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Download() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");  
+		PrintWriter out = response.getWriter();  
+		String filename = (String) request.getAttribute("onlyFileName");
+		filename="D:/Softwares/apache-tomcat-7.0.70-windows-x64/apache-tomcat-7.0.70/webapps/"+filename;
+		response.setContentType("APPLICATION/OCTET-STREAM");   
+		response.setHeader("Content-Disposition","attachment; filename=\"" + filename + "\"");   
+		  
+		FileInputStream fileInputStream = new FileInputStream(filename);  
+		            
+		int i;   
+		while ((i=fileInputStream.read()) != -1) {
+		out.write(i);   
+		}   
+		fileInputStream.close();   
+		out.close();   
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
